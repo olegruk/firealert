a52bafaedfc84ecc2b8ddbb8438788b41c0df7c3
olegruk <olegaruk@yandex.ru>
2021-02-19 14:17:09 +0300

    get_agol_features работает в полном объеме

--- get_agol_features.py
+++ get_agol_features.py
@@ -56,9 +56,9 @@ def return_token(service_url, username, password, request_url):
             else:
                 raise Exception("AGOL error: {} ".format(error_mess))
 
-def return_json(id, request_url, token, attr_fields, n):
+def return_json(id, request_url, token, attr_fields, count, n1, n2):
     params = {"token": token,
-            "where": "id>"+str(n)+" and id<="+str(n+1000),
+            "where": "id>"+str(n1)+" and id<="+str(n2),
             "objectIds": "",
             "time": "",
             "geometry": "",
@@ -76,7 +76,7 @@ def return_json(id, request_url, token, attr_fields, n):
             "gdbVersion": "",
             "returnDistinctValues": "false",
             "returnIdsOnly": "false",
-            "returnCountOnly": "false",
+            "returnCountOnly": count,
             "returnExtentOnly": "false",
             "orderByFields": "",
             "groupByFieldsForStatistics": "",
@@ -92,11 +92,34 @@ def return_json(id, request_url, token, attr_fields, n):
     response = submit_request(request)
     if "features" in response:
         return response
+    elif "count" in response:
+        return response.get('count')
     else:
         # Request for token must be made through HTTPS.
         if "error" in response:
             error_mess = response.get("error", {}).get("message")
             raise Exception("AGOL error: {} ".format(error_mess))
+
+#Удаляем таблицы
+def drop_tables(conn,cursor, tab):
+    log("Dropping table %s..." %tab)
+    sql_stat = "DROP TABLE IF EXISTS %s"%(tab)
+    try:
+        cursor.execute(sql_stat)
+        conn.commit()
+        log("Tables dropped.")
+    except IOError as e:
+        log('Error dropping table:$s'%e)
+
+def drop_temp_files(result_dir):
+    for the_file in os.listdir(result_dir):
+        file_path = os.path.join(result_dir, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            log('Cannot remove files:$s' %e)
 
 def get_agol_features_job():
  
@@ -113,30 +136,33 @@ def get_agol_features_job():
     
     #Создаем каталог для записи временных файлов
     result_dir = get_path(data_root,temp_folder)
-#    dst_file = os.path.join(result_dir,'temp.json')
-
-    #Получаем токен для доступа к сервису ArcGIS Online
-    token = return_token(service_url, username, password, request_url)
     
-    for id in [0]:
+    for id in [2, 3, 0, 1]:
+#    for id in [1]:
         log('Syncing for table №%s...'%(id))
+        #Получаем токен для доступа к сервису ArcGIS Online
+        token = return_token(service_url, username, password, request_url)
         n = 0
-        m = 0
-        is_repeat = True
-        while is_repeat:
-            response = return_json(id, request_url, token, attr_fields, n)
-            is_repeat = response["features"] != []
-            if is_repeat:
+        m = 1
+        loaded = 0
+        full_count = return_json(id, request_url, token, attr_fields, "true", 0, 100000000)
+#        print(full_count)
+        while loaded < full_count:
+            curr_count = return_json(id, request_url, token, attr_fields, "true", n, n+1000)
+            if curr_count > 0:
+                response = return_json(id, request_url, token, attr_fields, "false", n, n+1000)
                 dst_file = os.path.join(result_dir,'temp-%s.json'%m)
                 if id > 0:
                     response['geometryType'] = 'esriGeometryMultiPolygon'
-    #            is_repeat = (('exceededTransferLimit' in response) and ('gid' in response))
                 with open(dst_file, 'w') as outfile:
                     json.dump(response, outfile)
-                n+=1000
                 m+=1
-                log('%s loaded...'%(n))
-        for i in range(m):
+                loaded+=curr_count
+                log('Loaded: %(l)s, summary: %(s)s'%{'l': curr_count, 's': loaded})
+#                print('Loaded: %(l)s, summary: %(s)s'%{'l': curr_count, 's': loaded})
+            n+=1000
+        drop_tables(conn, cursor, dst_tabs[id])
+        for i in range(1, m):
             dst_file = os.path.join(result_dir,'temp-%s.json'%i)
             if i == 0:
                 command = """ogr2ogr -overwrite -update -t_srs "+proj=longlat +datum=WGS84 +no_defs" -f "PostgreSQL"  PG:"host=%(h)s user=%(u)s dbname=%(b)s password=%(w)s port=%(p)s" %(s)s -nln %(d)s"""%{'s':dst_file,'d':dst_tabs[id],'h':dbserver,'u':dbuser,'b':dbname,'w':dbpass,'p':dbport}
@@ -144,11 +170,13 @@ def get_agol_features_job():
                 command = """ogr2ogr -append -update -t_srs "+proj=longlat +datum=WGS84 +no_defs" -f "PostgreSQL"  PG:"host=%(h)s user=%(u)s dbname=%(b)s password=%(w)s port=%(p)s" %(s)s -nln %(d)s"""%{'s':dst_file,'d':dst_tabs[id],'h':dbserver,'u':dbuser,'b':dbname,'w':dbpass,'p':dbport}
             os.system(command)
             log('File %s uploaded...'%(dst_file))
+#            print('File %s uploaded...'%(dst_file))
 
-        log('All done in table №%(t)s. Total of %(c)s features.'%{'t': id, 'c': n})
+        log('All done in table №%(t)s. Total of %(c)s features.'%{'t': id, 'c':loaded})
+#        print('All done in table №%(t)s. Total of %(c)s features.'%{'t': id, 'c':loaded})
 
         cursor.execute("GRANT SELECT ON %(t)s TO %(u)s"%{'t':dst_tabs[id],'u':'db_reader'})
- 
+        drop_temp_files(result_dir)
 
     close_conn(conn, cursor)
 
