03693cfd20c32a360a80075f0645d41428209a65
olegruk <olegruk@yandex.ru>
2021-04-13 12:18:14 +0300

    Fixed statistic intervals in requester.py

--- requester.py
+++ requester.py
@@ -336,13 +336,13 @@ def check_reg_stat(reg, period, critical):
         SELECT count(*) FROM
             (SELECT name
             FROM %(y)s
-            WHERE date_time >= TIMESTAMP 'today' - INTERVAL '%(p)s' AND critical >= %(c)s AND region = '%(r)s') as critical_sel
+            WHERE date_time >= TIMESTAMP 'today' - INTERVAL '%(p)sh' AND date_time < TIMESTAMP 'today' AND critical >= %(c)s AND region = '%(r)s') as critical_sel
         """%{'y':year_tab,'p':period,'c':critical,'r':reg},
         """
         SELECT count(*) FROM
             (SELECT name
             FROM %(y)s
-            WHERE date_time >= TIMESTAMP 'today' - INTERVAL '%(p)s' AND region = '%(r)s') as all_sel
+            WHERE date_time >= TIMESTAMP 'today' - INTERVAL '%(p)sh' AND date_time < TIMESTAMP 'today' AND region = '%(r)s') as all_sel
         """%{'y':year_tab,'p':period,'r':reg}
         )
     try:
@@ -388,12 +388,12 @@ def check_zone_stat(zone, period):
         SELECT count(*) FROM
             (SELECT name
             FROM %(y)s
-            WHERE date_time >= TIMESTAMP 'today' - INTERVAL '%(p)s' AND vip_zone = '%(z)s' AND (vip_time IS NULL OR vip_time = '%(t)s')) as all_sel
+            WHERE date_time >= TIMESTAMP 'now' - INTERVAL '%(p)sh' AND vip_zone = '%(z)s' AND (vip_time IS NULL OR vip_time = '%(t)s')) as all_sel
         """%{'y':year_tab,'p':period,'z':zone,'t':zone_time},
         """
         UPDATE %(y)s SET
             vip_time = '%(t)s'
-        WHERE date_time >= TIMESTAMP 'today' - INTERVAL '%(p)s' AND vip_zone = '%(z)s' AND vip_time IS NULL
+        WHERE date_time >= TIMESTAMP 'now' - INTERVAL '%(p)sh' AND vip_zone = '%(z)s' AND vip_time IS NULL
         """%{'y':year_tab,'p':period,'z':zone,'t':zone_time}
         )
 
@@ -461,8 +461,8 @@ def new_alerts(period, cur_date):
         UPDATE %(a)s SET
             alert_date = '%(d)s',
             source = 'Робот'
-        WHERE alert_date IS NULL
-        """%{'a':alert_tab,'d':cur_date}
+        WHERE alert_date >= (TIMESTAMP 'today' - INTERVAL '%(p)s') AND alert_date < TIMESTAMP 'today' AND source IS NULL
+        """%{'a':alert_tab,'d':cur_date, 'p':period}
         )
 
     try:
